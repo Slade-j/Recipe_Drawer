30855e04fa9826928540a57fd52e5261ca2b694f
Jason Slade <jason.slade906@gmail.com>
2021-03-26 20:10:29 -0600

    added sign up and log in validations to the respective post routes as middleware ---- backend template fully tested and finished

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -38,4 +38,8 @@ router.use('/users', usersRouter);
 //     );
 // **************************************************************
 
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
 module.exports = router;
