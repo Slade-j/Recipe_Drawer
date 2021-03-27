32dec36dc1af3d93162d302be2092250a9e2e4c6
Jason Slade <jason.slade906@gmail.com>
2021-03-27 13:29:05 -0600

    added  signup from and functionality --- tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from "./store/session";
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,10 +15,13 @@ function App() {
       .then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return (
+  return isLoaded && (
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
