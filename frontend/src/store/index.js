698f69d4764f96abd845c6a41c82f43a8bc7a292
Jason Slade <jason.slade906@gmail.com>
2021-03-26 22:44:24 -0600

    set up Root and tested redux store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,7 +2,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-const roodReducer = combineReducers({
+const rootReducer = combineReducers({
 
 });
 
