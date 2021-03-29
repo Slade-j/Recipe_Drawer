3d425d9ad79d33dd85f9f43bc2d9ebc3b83ca89c
Jason Slade <jason.slade906@gmail.com>
2021-03-28 22:22:07 -0600

    able to test --- no bugs --- pushing completed template for future projects

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,8 +1,47 @@
 // frontend/src/components/Navigation/index.js
+
+// ************** Navigation without Modal *******************
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import './Navigation.css';
+
+// function Navigation({ isLoaded }){
+//   const sessionUser = useSelector(state => state.session.user);
+
+//   let sessionLinks;
+//   if (sessionUser) {
+//     sessionLinks = (
+//       <ProfileButton user={sessionUser} />
+//     );
+//   } else {
+//     sessionLinks = (
+//       <>
+//         <NavLink to="/login">Log In</NavLink>
+//         <NavLink to="/signup">Sign Up</NavLink>
+//       </>
+//     );
+//   }
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink exact to="/">Home</NavLink>
+//         {isLoaded && sessionLinks}
+//       </li>
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
+
+// ************** Navigation with Modal **********************
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -16,7 +55,7 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
+        <LoginFormModal />
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
