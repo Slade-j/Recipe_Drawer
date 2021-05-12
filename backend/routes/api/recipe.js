f9cdebe21f2d286cc7ee864bd2607c3b8dd16364
Jason Slade <jason.slade906@gmail.com>
2021-05-12 15:01:43 -0600

    able to create new recipe in database

--- backend/routes/api/recipe.js
+++ backend/routes/api/recipe.js
@@ -6,8 +6,16 @@ const { Recipe, User } = require('../../db/models');
 const router = express.Router();
 
 router.post('/', asyncHandler(async (req, res) => {
-  const { title, ingredients, instruction } = req.body;
-  console.log(title, ingredients, instruction, "checking req.body backend >>>>>>>>>>>")
+  const {
+    title,
+    ingredients,
+    directions,
+    originUrl,
+    userId
+  } = req.body;
+
+  const newRecipe = Recipe.createNewRecipe({ title, ingredients, directions, originUrl, userId })
+  return res.json({ newRecipe });
 }))
 
 module.exports = router;
