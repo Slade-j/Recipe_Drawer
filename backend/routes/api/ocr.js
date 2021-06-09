68bef6abd732439aceecf67e7244df7d8c209ded
Jason Slade <jason.slade906@gmail.com>
2021-06-09 00:53:43 -0600

    implementd microsoft vision via npm package

--- backend/routes/api/ocr.js
+++ backend/routes/api/ocr.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const ocrSpace = require('ocr-space-api-wrapper');
+const microsofComputerVision = require("microsoft-computer-vision")
 const fetch = require('node-fetch');
 const {
   s3,
@@ -18,27 +19,27 @@ const key = process.env.API_KEY
 // aws included here in-order to get a working URL to pass to ocr.open wrapper
 // if another ocr api is used in futre iterations it will be ideal to seperate
 // aws from this post
-router.post('/', singleMulterUpload('image'), asyncHandler(async (req, res) => {
-  console.log("HITHITHIT")
-  const { mimetype } = await req.file;
-  console.log(mimetype, "MIMETYPE>>>>>>>>>>>>>")
-  try {
-    // placing image in s3 bucket, getting url in return
-    const url = await singlePublicFileUpload(req.file);
-    // must set filetype because of default content-type comming back from aws
-    console.log(url, "URL>>>>>>>>>>>>>>>>>>>>>")
-    const response = await ocrSpace(
-      `${url}`, {apiKey: key, url: 'url', filetype: mimetype }
-    )
-    console.log(response, "BACK FROM  OCR>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    return res.json({ response, url });
-   } catch (error) {
-    console.error(error)
-   }
+// router.post('/', singleMulterUpload('image'), asyncHandler(async (req, res) => {
 
-}))
+//   const { mimetype } = await req.file;
 
-module.exports = router;
+//   try {
+//     // placing image in s3 bucket, getting url in return
+//     const url = await singlePublicFileUpload(req.file);
+//     // must set filetype because of default content-type comming back from aws
+
+//     const response = await ocrSpace(
+//       `${url}`, {apiKey: key, url: 'url', filetype: mimetype }
+//     )
+
+//     return res.json({ response, url });
+//    } catch (error) {
+//     console.error(error)
+//    }
+
+// }))
+
+
 
 
 // const ingredients = await fetch("https://zestful.p.rapidapi.com/parseIngredients", {
@@ -52,3 +53,82 @@ module.exports = router;
 // 		"ingredients": text[0]["ParsedText"]
 // 	}
 // })
+
+router.post('/', singleMulterUpload('image'), asyncHandler(async (req, res) => {
+
+  const { mimetype } = await req.file;
+
+  try {
+    // placing image in s3 bucket, getting url in return
+    const url = await singlePublicFileUpload(req.file);
+    // must set filetype because of default content-type comming back from aws
+
+    const response = await microsofComputerVision.orcImage({
+      "Ocp-Apim-Subscription-Key": "8018252732464058a1bfea1480876b04",
+      "request-origin":"westus",
+      "content-type": "application/json",
+      "url": `${url}`,
+      "language": "en",
+      "detect-orientation": true
+  })
+
+    let returnLines = response.regions.map(object => object.lines)
+    let lineArray = []
+    returnLines.forEach(line => {
+      let eachLine = []
+      line.forEach(object => {
+        object.words.forEach(textObj => {
+          eachLine = [...eachLine, textObj.text]
+        })
+        lineArray.push(eachLine)
+      })
+    })
+
+    console.log(lineArray)
+    // return res.json({ response, url });
+   } catch (error) {
+    console.error(error)
+   }
+
+}))
+
+// microsofComputerVision.orcImage({
+//     "Ocp-Apim-Subscription-Key": "<your-subscription-key>",
+//     "request-origin":"<Choose-one-from-Supported-Regions>",
+//     "content-type": "application/json",
+//     "url": "http://cdn.quotesgram.com/img/81/49/660235022-Random-Funny-Quotes-.jpg",
+//     "language": "en",
+//     "detect-orientation": true
+// }).then((result)=>{
+
+//   console.log(JSON.stringify(result))        // {
+                              //     "language": "en",
+                              //     "textAngle": 0,
+                              //     "orientation": "Up",
+                              //     "regions": [
+                              //         {
+                              //             "boundingBox": "7,55,605,387",
+                              //             "lines": [
+                              //                 {
+                              //                     "boundingBox": "7,55,603,65",
+                              //                     "words": [
+                              //                         {
+                              //                             "boundingBox": "7,59,291,61",
+                              //                             "text": "HOME:"
+                              //                         },
+                              //                         {
+                              //                             "boundingBox": "326,55,284,65",
+                              //                             "text": "Where"
+                              //                         }
+                              //                     ]
+                              //                 },
+                              //                 ...
+                              //             ]
+                              //         }
+                              //     ]
+                              // }
+// }).catch((err)=>{
+//   throw err
+// })
+
+module.exports = router;
